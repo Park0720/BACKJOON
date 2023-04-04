694e9531027ac820fc44a65bed45bb39985c29f6
Park0720 <ppmm98@naver.com>
2023-04-04 17:58:33 +0900

    B10950

---
+++ src/main/java/org/example/step3/B10950.java
@@ -0,0 +1,15 @@
+package org.example.step3;
+
+import java.util.Scanner;
+
+public class B10950 {
+    public static void main(String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+        for (int i = 0; i < T; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
