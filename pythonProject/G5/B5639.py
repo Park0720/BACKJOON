41979cbbb45c452d9708e8b73a56c2d28f7fc55e
박민준 <ppmm98@naver.com>
2024-03-28 11:41:17 +0900

    G5

--- pythonProject/G5/B5639.py
+++ pythonProject/G5/B5639.py
@@ -1,5 +1,31 @@
-input_list = []
-for i in range(10000):
-    input_list.append(int(input()))
+import sys
+sys.setrecursionlimit(10**6)
 
-print(input_list)
+
+def pre_to_post(start, end):
+    if start > end:
+        return
+
+    root = pre_order_list[start]
+    idx = start + 1
+
+    while idx <= end:
+        if pre_order_list[idx] > root:
+            break
+        idx += 1
+
+    pre_to_post(start+1, idx-1)
+    pre_to_post(idx, end)
+    print(root)
+
+
+pre_order_list = []
+while True:
+    try:
+        pre_order_list.append(int(input()))
+    except:
+        break
+
+in_order_list = sorted(pre_order_list)
+
+pre_to_post(0, len(pre_order_list)-1)
