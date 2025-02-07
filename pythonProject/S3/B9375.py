c2c0bef0262b37ce5ffc79d1fbeab054fe5ec83b
박민준 <ppmm98@naver.com>
2025-02-07 19:50:24 +0900

    B9375

--- pythonProject/S3/B9375.py
+++ pythonProject/S3/B9375.py
@@ -3,4 +3,20 @@ import sys
 T = int(sys.stdin.readline().strip())
 
 for _ in range(T):
+    case = {}
+    result = 1
+
     n = int(sys.stdin.readline().strip())
+
+    for _ in range(n):
+        wear, wear_type = sys.stdin.readline().strip().split()
+
+        if not wear_type in case:
+            case[wear_type] = 1
+        else:
+            case[wear_type] += 1
+
+    for i in case:
+        result *= (case[i] + 1)
+
+    print(result-1)
