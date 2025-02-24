2d0f894a0cd2a0d2124373a21e887257c9626372
박민준 <ppmm98@naver.com>
2025-02-25 00:08:17 +0900

    B17626

---
+++ pythonProject/S3/B17626.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().strip())
+count = 0
+
+DP = [0] * (n + 1)
+
+k = 1
+while k**2 <= n:
+    DP[k**2] = 1
+    k += 1
+
+for i in range(1, n + 1):
+    if DP[i] != 0:
+        continue
+    j = 1
+    while j*j <= i:
+        if DP[i] == 0:
+            DP[i] = DP[j*j] + DP[i - j*j]
+        else:
+            DP[i] = min(DP[i], DP[j*j] + DP[i - j*j])
+        j += 1
+print(DP[n])
